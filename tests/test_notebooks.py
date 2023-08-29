c1d77c2b173e7624c5902b393e378cd3ea4e2436
Christian Klauer <chr.klauer@gmail.com>
2023-08-30 00:44:15 +0200

    .

---
+++ tests/test_notebooks.py
@@ -0,0 +1,16 @@
+
+# example_test.py
+from testbook import testbook
+
+
+@testbook('examples/sysident.ipynb', execute=True)
+def test_sysident(tb):
+   assert True
+
+@testbook('examples/state_est_pendulum.ipynb', execute=True)
+def test_state_est_pendulum(tb):
+   assert True
+
+@testbook('examples/trajectory_optim.ipynb', execute=True)
+def test_trajectory_optim(tb):
+   assert True
