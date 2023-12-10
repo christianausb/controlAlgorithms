2defba6ddfa40adb2ffe97a082bc6fc9b1d62cdb
Christian Klauer <chr.klauer@gmail.com>
2023-12-10 16:52:48 +0100

    new api - I

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -18,16 +18,15 @@ def test_state_est_pendulum(tb):
 
 @testbook('examples/trajectory_optim_integrator.ipynb', execute=True)
 def test_trajectory_optim(tb):
-#   verify_test_results(tb.test_results)
    assert True
 
 @testbook('examples/trajectory_optim_cart_pendulum.ipynb', execute=True)
 def test_trajectory_optim(tb):
    assert True
 
-@testbook('examples/trajectory_optim.ipynb', execute=True)
-def test_trajectory_optim(tb):
-   assert True
+# @testbook('examples/trajectory_optim.ipynb', execute=True)
+# def test_trajectory_optim(tb):
+#    assert True
 
 @testbook('examples/trajectory_optim_flow.ipynb', execute=True)
 def test_trajectory_optim_flow(tb):
